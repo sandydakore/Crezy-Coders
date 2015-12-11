4a08926f91c11644b02e7eac5980f7171833ec20
sandydakore <sandipkumar.patel@robertsoncollege.net>
2015-12-10 18:21:45 -0600

    Email Verification

--- MVCLogin/MVCLogin/App_Start/Startup.Auth.cs
+++ MVCLogin/MVCLogin/App_Start/Startup.Auth.cs
@@ -17,8 +17,8 @@ namespace MVCLogin
             // Configure the db context, user manager and signin manager to use a single instance per request
             app.CreatePerOwinContext(ApplicationDbContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
-            app.CreatePerOwinContext<ApplicationRoleManager>(ApplicationRoleManager.Create);
             app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
+            app.CreatePerOwinContext<ApplicationRoleManager>(ApplicationRoleManager.Create);
 
             // Enable the application to use a cookie to store information for the signed in user
             // and to use a cookie to temporarily store information about a user logging in with a third party login provider
