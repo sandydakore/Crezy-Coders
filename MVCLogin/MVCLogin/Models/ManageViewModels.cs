40fecacc6de187bc38f01dd74c58c27d83356e8b
sandydakore <sandipkumar.patel@robertsoncollege.net>
2015-12-11 13:16:49 -0600

    Remove Unneccessary Code

--- MVCLogin/MVCLogin/Models/ManageViewModels.cs
+++ MVCLogin/MVCLogin/Models/ManageViewModels.cs
@@ -8,9 +8,7 @@ namespace MVCLogin.Models
     public class IndexViewModel
     {
         public bool HasPassword { get; set; }
-        public IList<UserLoginInfo> Logins { get; set; }
-        public string PhoneNumber { get; set; }
-        public bool TwoFactor { get; set; }
+        public IList<UserLoginInfo> Logins { get; set; }       
         public bool BrowserRemembered { get; set; }
     }
 
@@ -18,11 +16,6 @@ namespace MVCLogin.Models
     {
         public IList<UserLoginInfo> CurrentLogins { get; set; }
         public IList<AuthenticationDescription> OtherLogins { get; set; }
-    }
-
-    public class FactorViewModel
-    {
-        public string Purpose { get; set; }
     }
 
     public class SetPasswordViewModel
@@ -56,31 +49,5 @@ namespace MVCLogin.Models
         [Display(Name = "Confirm new password")]
         [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
-    }
-
-    public class AddPhoneNumberViewModel
-    {
-        [Required]
-        [Phone]
-        [Display(Name = "Phone Number")]
-        public string Number { get; set; }
-    }
-
-    public class VerifyPhoneNumberViewModel
-    {
-        [Required]
-        [Display(Name = "Code")]
-        public string Code { get; set; }
-
-        [Required]
-        [Phone]
-        [Display(Name = "Phone Number")]
-        public string PhoneNumber { get; set; }
-    }
-
-    public class ConfigureTwoFactorViewModel
-    {
-        public string SelectedProvider { get; set; }
-        public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; }
-    }
+    }   
 }
