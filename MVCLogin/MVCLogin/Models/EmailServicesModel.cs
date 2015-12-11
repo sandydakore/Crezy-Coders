7580c0e74f99e7eafba76de3ce04d568a4b0c695
sandydakore <sandipkumar.patel@robertsoncollege.net>
2015-12-10 18:24:40 -0600

    Email Verification

---
+++ MVCLogin/MVCLogin/Models/EmailServicesModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+
+namespace MVCLogin.Models
+{
+    public class EmailServicesModel : SmtpClient
+    {
+        // Gmail user-name
+        public string UserName { get; set; }
+
+        public EmailServicesModel() :
+            base(ConfigurationManager.AppSettings["GmailHost"], Int32.Parse(ConfigurationManager.AppSettings["GmailPort"]))
+        {
+            //Get values from web.config file:
+            this.UserName = ConfigurationManager.AppSettings["GmailUserName"];
+            this.EnableSsl = Boolean.Parse(ConfigurationManager.AppSettings["GmailSsl"]);
+            this.UseDefaultCredentials = false;
+            this.Credentials = new System.Net.NetworkCredential(this.UserName, ConfigurationManager.AppSettings["GmailPassword"]);
+        }
+    }
+}
