b3ee2cc8ceb9cf151bd2bf85eeff1114146097a6
sandydakore <sandipkumar.patel@robertsoncollege.net>
2015-12-22 15:38:02 -0600

    Add Blogs and Comments on Home Index

--- SDStudentPortal/SDStudentPortal/Controllers/HomeController.cs
+++ SDStudentPortal/SDStudentPortal/Controllers/HomeController.cs
@@ -1,16 +1,26 @@
-﻿using System;
+﻿using SDStudentPortal.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using System.Data.Entity;
 
 namespace SDStudentPortal.Controllers
 {
     public class HomeController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
-            return View();
+            IEnumerable<Blog> blogs = db.blog.OrderByDescending(b => b.BlogCreatedDate).ToList();
+            IEnumerable<BlogComment> comments = db.blogcomment.ToList();
+
+            var tuple = new Tuple<IEnumerable<Blog>, IEnumerable<BlogComment>>(blogs, comments);
+
+            return View(tuple);
         }
 
         public ActionResult About()
