fcc5cc6b5ad20c68f7bddf0884f2b13e02814a2d
sandydakore <sandipkumar.patel@robertsoncollege.net>
2015-12-22 15:36:04 -0600

    Add Blog Model

---
+++ SDStudentPortal/SDStudentPortal/Models/Blog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SDStudentPortal.Models
+{
+    public class Blog
+    {
+        public int BlogID { get; set; }
+
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Content { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime BlogCreatedDate { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime BlogUpdatedDate { get; set; }
+
+        public bool Public { get; set; }
+    }
+}
