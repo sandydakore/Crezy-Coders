f01b836c6e3b1baae3d864c18023c8bc593ef772
sandydakore <sandipkumar.patel@robertsoncollege.net>
2016-01-06 13:54:52 -0600

    datepicker

--- SDStudentPortal/SDStudentPortal/App_Start/BundleConfig.cs
+++ SDStudentPortal/SDStudentPortal/App_Start/BundleConfig.cs
@@ -10,6 +10,9 @@ namespace SDStudentPortal
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
+            
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+            //            "~/Scripts/jquery-ui.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -22,9 +25,6 @@ namespace SDStudentPortal
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/datepicker").Include(
-                      "~/Scripts/DatePickerReady.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
