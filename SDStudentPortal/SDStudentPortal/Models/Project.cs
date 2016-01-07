b6632c4df0f544915b2680f53020c2e20d66e4c8
sandydakore <sandipkumar.patel@robertsoncollege.net>
2016-01-07 16:15:53 -0600

    Add project

---
+++ SDStudentPortal/SDStudentPortal/Models/Project.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+
+namespace SDStudentPortal.Models
+{
+    public class Project
+    {
+        public int ProjectID { get; set; }
+
+        [ForeignKey("User")]
+        public virtual string UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public virtual ICollection<ProjectFile> files { get; set; }
+    }
+}
